c088b95e2ef90d3b3fef779fea9108a120cf0520
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-02-22 15:35:35 +0100

    searching for a node using more features than just its ID

--- lib/Treex/Block/PCEDT20Coref/CorefLinksLoader.pm
+++ lib/Treex/Block/PCEDT20Coref/CorefLinksLoader.pm
@@ -20,26 +20,60 @@ sub _build_coref_links {
     while (my $line = <FILE>) {
         chomp $line;
         my ($doc_id, @rest) = split /\t/, $line;
+        my $type = pop @rest;
+        my @anaph_feats = splice @rest, 0, scalar(@rest)/2;
         if (defined $coref_links->{$doc_id}) {
-            push @{$coref_links->{$doc_id}}, \@rest;
+            push @{$coref_links->{$doc_id}}, [ \@anaph_feats, \@rest, $type ];
         }
         else {
-            $coref_links->{$doc_id} = [ \@rest ];
+            $coref_links->{$doc_id} = [ [ \@anaph_feats, \@rest, $type] ];
         }
     }
     close FILE;
     return $coref_links;
 }
 
+sub find_node {
+    my ($doc, $id, $par_id, $tlemma, $functor, $form) = @_;
+    # find by its id
+    my $node;
+    if ($doc->id_is_indexed($id)) {
+        $node = $doc->get_node_by_id($id);
+        return $node if (defined $node);
+    }
+    if ($doc->id_is_indexed($par_id)) {
+        my $par_node = $doc->get_node_by_id($par_id);
+        my @descs = $par_node->get_descendants;
+        # find a changed generated node, e.g., #Gen -> #PersPron, #PersPron -> #Compar and #PersPron -> #Cor
+        if ($tlemma =~ /^#/) {
+            ($node) = grep {$_->t_lemma =~ /^#/ && $_->functor eq $functor} @descs;
+            return $node if (defined $node);
+        }
+        # find the hyphen-compounds, e.g., 'third-quartal', 'Miami-based'  -> in the old PCEDT they are represented as a single node, in the new one as three anodes (two tnodes)
+        ($node, my @rest) = grep {my $anode = $_->get_lex_anode; defined $anode ? $anode->form =~ /$form/ : 0 } @descs;
+        if (@rest) {
+            log_warn "More than one node match the form $form among the descendants of $par_id";
+        }
+        return $node if (defined $node);
+    }
+    else {
+        log_warn "Parent $par_id not defined";
+    }
+    log_warn "The node $id not found";
+}
+
 sub process_document {
     my ($self, $doc) = @_;
-    print STDERR $doc->file_stem."\n";
-    foreach my $triple (@{$self->_coref_links->{$doc->file_stem}}) {
-        my ($anaph_id, $ante_id, $type) = @$triple;
-        my $anaph = $doc->get_node_by_id($anaph_id);
+    foreach my $tuple (@{$self->_coref_links->{$doc->file_stem}}) {
+        my ($anaph_feats, $ante_feats, $type) = @$tuple;
+        my $anaph = find_node($doc, @$anaph_feats);
         next if (!defined $anaph);
-        my $ante = $doc->get_node_by_id($ante_id);
+        my $ante = find_node($doc, @$ante_feats);
         next if (!defined $ante);
+        if ($anaph->t_lemma eq "#Gen") {
+            log_info "A link that would originate from a #Gen node found.";
+            #next;
+        }
         if ($type eq "text") {
             $anaph->add_coref_text_nodes($ante);
         }
