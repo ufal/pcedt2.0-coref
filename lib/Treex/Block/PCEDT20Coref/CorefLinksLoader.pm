431cf74a92012d6edecf06dec64a93bb16179f96
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-02-23 09:57:52 +0100

    counting the position of the charactrs - it's a dead end, since the number of  characters may differ due to typos

--- lib/Treex/Block/PCEDT20Coref/CorefLinksLoader.pm
+++ lib/Treex/Block/PCEDT20Coref/CorefLinksLoader.pm
@@ -34,41 +34,69 @@ sub _build_coref_links {
 }
 
 sub find_node {
-    my ($doc, $id, $par_id, $tlemma, $functor, $form) = @_;
+    my ($doc, $xy_to_anode, $id, $par_id, $tlemma, $functor, $xy) = @_;
     # find by its id
     my $node;
     if ($doc->id_is_indexed($id)) {
         $node = $doc->get_node_by_id($id);
         return $node if (defined $node);
     }
-    if ($doc->id_is_indexed($par_id)) {
+    # find a changed generated node, e.g., #Gen -> #PersPron, #PersPron -> #Compar and #PersPron -> #Cor
+    if ($tlemma =~ /^#/) {
         my $par_node = $doc->get_node_by_id($par_id);
         my @descs = $par_node->get_descendants;
-        # find a changed generated node, e.g., #Gen -> #PersPron, #PersPron -> #Compar and #PersPron -> #Cor
-        if ($tlemma =~ /^#/) {
-            ($node) = grep {$_->t_lemma =~ /^#/ && $_->functor eq $functor} @descs;
-            return $node if (defined $node);
+        ($node) = grep {$_->t_lemma =~ /^#/ && $_->functor eq $functor} @descs;
+        if (defined $node) {
+            return $node;
         }
-        # find the hyphen-compounds, e.g., 'third-quartal', 'Miami-based'  -> in the old PCEDT they are represented as a single node, in the new one as three anodes (two tnodes)
-        ($node, my @rest) = grep {my $anode = $_->get_lex_anode; defined $anode ? $anode->form =~ /$form/ : 0 } @descs;
-        if (@rest) {
-            log_warn "More than one node match the form $form among the descendants of $par_id";
+        else {
+            log_warn "No node found for a generated node $id";
         }
-        return $node if (defined $node);
     }
+    # find a changed surface node
+    # e.g., the hyphen-compounds: 'third-quartal', 'Miami-based', etc.  -> in the old PCEDT they are represented as a single node, in the new one as three anodes (two tnodes)
     else {
-        log_warn "Parent $par_id not defined";
+        my $lang = 'cs';
+        if ($id =~ /English/) {
+            $lang = 'en';
+        }
+        my $anode = $xy_to_anode->{$lang}{$xy};
+        if (!defined $anode) {
+            print STDERR "LANG: $lang, XY: $xy\n";
+        }
+        ($node) = $anode->get_referencing_nodes('a/lex.rf');
+        if (defined $node) {
+            return $node;
+        }
+        else {
+            log_warn "No node found for a surface node $id";
+        }
     }
-    log_warn "The node $id not found";
 }
 
 sub process_document {
     my ($self, $doc) = @_;
+
+    my %xy_to_id = ();
+    my @bundles = $doc->get_bundles();
+    for (my $x = 0; $x < @bundles; $x++) {
+        foreach my $zone ($bundles[$x]->get_all_zones()) {
+            if (($zone->selector // '') eq ($self->selector // '')) {
+                my @anodes = grep {defined $_->afun} $zone->get_atree->get_descendants({ordered => 1});
+                my $y = 0;
+                foreach my $anode (@anodes) {
+                    #print STDERR "ANODE: " . $anode->id . ", $x:$y\n";
+                    $xy_to_id{$anode->language}{"$x:$y"} = $anode;
+                    $y += length($anode->form);
+                }
+            }
+        }
+    }
     foreach my $tuple (@{$self->_coref_links->{$doc->file_stem}}) {
         my ($anaph_feats, $ante_feats, $type) = @$tuple;
-        my $anaph = find_node($doc, @$anaph_feats);
+        my $anaph = find_node($doc, \%xy_to_id, @$anaph_feats);
         next if (!defined $anaph);
-        my $ante = find_node($doc, @$ante_feats);
+        my $ante = find_node($doc, \%xy_to_id, @$ante_feats);
         next if (!defined $ante);
         if ($anaph->t_lemma eq "#Gen") {
             log_info "A link that would originate from a #Gen node found.";
--- lib/Treex/Block/PCEDT20Coref/CorefLinksPrinter.pm
+++ lib/Treex/Block/PCEDT20Coref/CorefLinksPrinter.pm
@@ -5,13 +5,19 @@ use Treex::Core::Common;
 
 extends 'Treex::Core::Block';
 
+has '_id_to_xy' => ( is => 'rw', isa => 'HashRef' );
+
 sub print_link { 
-    my ($anaph, $ante, $type) = @_;
+    my ($self, $anaph, $ante, $type) = @_;
     print $anaph->get_document->file_stem . "\t";
     print join "\t", map {
         my @feats = ($_->id, $_->get_parent->id, $_->t_lemma, $_->functor);
         my $anode = $_->get_lex_anode;
-        push @feats, defined $anode ? $anode->form : "";
+        #if (defined $anode) {
+        #    print STDERR "ANODE_ID: ". $anode->id . ", " . $self->_id_to_xy->{$anode->id} . "\n";
+        #}
+        push @feats, defined $anode ? $self->_id_to_xy->{$anode->id} : "";
+        #push @feats, defined $anode ? $anode->form : "";
         @feats
     } ($anaph, $ante);
     print "\t".$type."\n";
@@ -20,9 +26,29 @@ sub print_link {
 sub process_tnode {
     my ($self, $tnode) = @_;
     my @text = $tnode->get_coref_text_nodes;
-    print_link($tnode, $_, "text") foreach (@text);
+    $self->print_link($tnode, $_, "text") foreach (@text);
     my @gram = $tnode->get_coref_gram_nodes;
-    print_link($tnode, $_, "gram") foreach (@gram);
+    $self->print_link($tnode, $_, "gram") foreach (@gram);
 }
+
+before 'process_document' => sub {
+    my ($self, $doc) = @_;
+    my %id_to_xy = ();
+    my @bundles = $doc->get_bundles();
+    for (my $x = 0; $x < @bundles; $x++) {
+        foreach my $zone ($bundles[$x]->get_all_zones()) {
+            if (($zone->selector // '') eq ($self->selector // '')) {
+                my @anodes = grep {defined $_->afun} $zone->get_atree->get_descendants({ordered => 1});
+                my $y = 0;
+                foreach my $anode (@anodes) {
+                    #print STDERR "ANODE: " . $anode->id . ", $x:$y\n";
+                    $id_to_xy{$anode->id} = $x.":".$y;
+                    $y += length($anode->form);
+                }
+            }
+        }
+    }
+    $self->_set_id_to_xy(\%id_to_xy);
+};
 
 1;
