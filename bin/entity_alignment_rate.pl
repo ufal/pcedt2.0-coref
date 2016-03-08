d97586ea83d6a3eda26da37372940274c7ad027c
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-03-08 01:12:13 +0100

    entity alignment rate included; also a number of aligned antecedent, which will not be used in the paper, though

---
+++ bin/entity_alignment_rate.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use List::Util qw/sum max/;
+
+my $ali_type = $ARGV[0];
+
+my %entity_aligns = ();
+
+while (my $line = <STDIN>) {
+    chomp $line;
+    my @cols = split /\t/, $line;
+    my ($docid) = ($cols[0] =~ /(wsj_?....)/);
+    $line =~ /TNODE_ENTITY=(\d+)/;
+    next if (!$1);
+    my $tnode_entity = $docid."_".$1;
+    #print STDERR "TNODE_ENTITY: $tnode_entity\n";
+    my @ali_entities = ();
+    foreach my $col (@cols) {
+        my ($ali_entity, $type) = ($col =~ /^ALI_ENTITY=(\d+):(\S+)/);
+        next if (!$ali_entity);
+        #next if ($ali_entity == 0);
+        if ($line =~ /IS_ALIGN_COREF=1/) {
+            next if ($ali_type eq "unsup" && $type =~ /coref_(supervised|gold)/);
+            next if ($ali_type eq "sup" && $type !~ /coref_supervised/);
+            next if ($ali_type eq "gold" && $type !~ /coref_gold/);
+        }
+        push @ali_entities, $ali_entity;
+    }
+    $entity_aligns{$tnode_entity}{$_}++ foreach (@ali_entities);
+    $entity_aligns{$tnode_entity}{0}++ if (!@ali_entities);
+}
+
+#use Data::Dumper;
+#print STDERR Dumper(\%entity_aligns);
+
+my $entity_rate_sum = 0;
+foreach my $entity_id (sort keys %entity_aligns) {
+    my $sum = sum values %{$entity_aligns{$entity_id}};
+    my %no_zero = map {$_ => $entity_aligns{$entity_id}{$_}} grep {$_} keys %{$entity_aligns{$entity_id}};
+    my $max = max(values %no_zero) // 0;
+    $entity_rate_sum += ($max / $sum);
+    #print STDERR "$entity_id => $entity_rate_sum ( $max / $sum )\n";
+}
+print "ENTITY ALIGN RATE: " . ($entity_rate_sum / scalar(keys %entity_aligns)) . "\n";
--- lib/Treex/Block/PCEDT20Coref/AlignCorefStats.pm
+++ lib/Treex/Block/PCEDT20Coref/AlignCorefStats.pm
@@ -2,16 +2,42 @@ package Treex::Block::PCEDT20Coref::AlignCorefStats;
 
 use Moose;
 use Treex::Core::Common;
+use Treex::Tool::Coreference::Utils;
 
 extends 'Treex::Core::Block';
+
+has '_id_to_entity' => ( is => 'rw', isa => 'HashRef' );
+
+sub id_to_entity {
+    my ($doc, $lang, $selector) = @_;
+    my @ttrees = map {$_->get_tree($lang, 't', $selector)} $doc->get_bundles;
+    my @chains = Treex::Tool::Coreference::Utils::get_coreference_entities(\@ttrees);
+    my $entity_id = 1;
+    my %id_to_entity = ();
+    foreach my $entity (@chains) {
+        $id_to_entity{$_->id} = $entity_id foreach (@$entity);
+        $entity_id++;
+    }
+    return \%id_to_entity;
+}
+
+before 'process_document' => sub {
+    my ($self, $doc) = @_;
+    my $id_to_entity = {
+        en => id_to_entity($doc, 'en', $self->selector),
+        cs => id_to_entity($doc, 'cs', $self->selector),
+    };
+    $self->_set_id_to_entity($id_to_entity);
+};
 
 sub coref_types {
     my ($tnode) = @_;
     my @types = ();
-    if ($tnode->get_coref_text_nodes) {
+    my @antes = ();
+    if (@antes = $tnode->get_coref_text_nodes) {
         push @types, "COREF_TEXT";
     }
-    if ($tnode->get_coref_gram_nodes) {
+    if (@antes = $tnode->get_coref_gram_nodes) {
         push @types, "COREF_GRAM";
     }
     if ($tnode->get_attr('coref_special')) {
@@ -20,24 +46,44 @@ sub coref_types {
     if (!@types) {
         push @types, "OTHER";
     }
-    return join "+", @types;
+    return (join("+", @types), \@antes);
 }
 
 sub process_tnode {
     my ($self, $tnode) = @_;
 
-    my $types = "TNODE=" . coref_types($tnode);
+    my ($types, $antes) = coref_types($tnode);
+    $types = "TNODE=" . $types;
     return if ($types eq "TNODE=OTHER");
 
     my $is_align_coref = "IS_ALIGN_COREF=" . ($tnode->get_attr('is_align_coref') ? 1 : 0);
 
-    my ($ali_tnodes, $ali_types) = $tnode->get_undirected_aligned_nodes();
-    my @ali_info = map {
-        my $ali_tnode_types = coref_types($ali_tnodes->[$_]);
-        "ALI_TNODE=".$ali_tnode_types.":".$ali_types->[$_];
-    } 0..$#$ali_tnodes;
+    my $tnode_entity = "TNODE_ENTITY=".($self->_id_to_entity->{$tnode->language}{$tnode->id} // "0");
 
-    print join "\t", ($tnode->id, $types, $is_align_coref, @ali_info);
+    my ($ali_tnodes, $ali_types) = $tnode->get_undirected_aligned_nodes();
+    my @ali_info = ();
+    my @ante_ali_info = ();
+    my @ali_entities = ();
+    for (my $i = 0; $i < @$ali_tnodes; $i++) {
+        my ($ali_tnode_types, $ali_tnode_antes) = coref_types($ali_tnodes->[$i]);
+        push @ali_info, "ALI_TNODE=".$ali_tnode_types.":".$ali_types->[$i];
+        foreach my $l1_ante (@$antes) {
+            foreach my $l2_ante (@$ali_tnode_antes) {
+                my ($ali_antes, $ali_antes_types) = $l1_ante->get_undirected_aligned_nodes();
+                my @matching_idxs = grep {$ali_antes->[$_] == $l2_ante} 0..$#$ali_antes;
+                foreach my $match_idx (@matching_idxs) {
+                    push @ante_ali_info, "ANTES_ALI=".$ali_antes_types->[$match_idx];
+                }
+            }
+        }
+        push @ali_entities, 
+            "ALI_ENTITY=".
+            ($self->_id_to_entity->{$ali_tnodes->[$i]->language}{$ali_tnodes->[$i]->id} // "0").
+            ":".
+            $ali_types->[$i];
+    }
+
+    print join "\t", ($tnode->id, $types, $is_align_coref, $tnode_entity, @ali_info, @ante_ali_info, @ali_entities);
     print "\n";
 }
 
