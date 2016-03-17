e7c190842ab4b43b0dfc5ebae2add2b4c3e4ddb8
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-03-17 15:24:51 +0100

    indexing texts by name + titles added

--- docs/fill_texts_to_patterns.pl
+++ docs/fill_texts_to_patterns.pl
@@ -7,26 +7,34 @@ use open qw(:std :utf8);
 my %texts_in_file = ();
 foreach my $text_path (@ARGV) {
     open my $text_f, "<:utf8", $text_path;
-    my @texts = ();
+    my %texts = ();
+    my $curr_id;
     my $curr_text = "";
     while (<$text_f>) {
-        if ($_ =~ /^\s*$/ && $curr_text !~ /^\s*$/s) {
+        if ($_ =~ /^\s*$/ && $curr_text !~ /^\s*$/s && defined $curr_id) {
             chomp $curr_text;
-            push @texts, $curr_text;
+            $texts{$curr_id} = $curr_text;
             $curr_text = "";
+            $curr_id = undef;
             next;
         }
-        $curr_text .= $_;
+        if (!defined $curr_id) {
+            chomp $_;
+            $curr_id = $_;
+        }
+        else {
+            $curr_text .= $_;
+        }
     }
     chomp $curr_text;
-    push @texts, $curr_text;
+    $texts{$curr_id} = $curr_text;
     $text_path =~ s|^.*/([^/]*)$|$1|;
-    $texts_in_file{$text_path} = \@texts;
+    $texts_in_file{$text_path} = \%texts;
 }
 
 while (<STDIN>) {
     while ($_ =~ /__TEXT:([^#]*)##([^_]*)__/) {
-        my $text = $texts_in_file{$1}[$2-1];
+        my $text = $texts_in_file{$1}{$2};
         $_ =~ s/__TEXT:$1##$2__/$text/;
     }
     print $_;
